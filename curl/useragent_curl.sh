862a593ef0b005e19225a54634508902b15d83a5
k22o <kana.t3.105@gmail.com>
2021-11-09 20:14:27 +0900

    update

--- curl/useragent_curl.sh
+++ curl/useragent_curl.sh
@@ -25,3 +25,6 @@ refOption="-e https://test-referer.com/"
 command="curl ${uaOption} ${refOption} ${dataOption} ${url} ${saveOption} ${requestOption}"
 echo ${command} >> execute.sh
 
+# sample
+## curl -A "Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1) -e https://test-referer.com/ -I https://www.google.com
+## curl -X POST -d '{"id":"12", "number":"100"}' localhost:8080/api/v1/service
