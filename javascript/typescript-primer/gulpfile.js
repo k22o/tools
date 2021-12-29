c7e8d3516da198f5edc735ba8f534c80be8775e9
k22o <kana_t3@yahoo.co.jp>
2021-12-29 15:34:58 +0900

    new files

--- javascript/typescript-primer/gulpfile.js
+++ javascript/typescript-primer/gulpfile.js
@@ -1,11 +1,13 @@
-const { dest, src, task} = require("gulp");
+const { dest, src, series, watch} = require("gulp");
 const ts = require("gulp-typescript");
 const tsProject = ts.createProject("./tsconfig.json");
 
+const srcFiles = './src/*.ts';
+
 function buildTs() {
-    return src('./src/**.ts')
+    return src(srcFiles)
     .pipe(tsProject())
     .pipe(dest("./dist"));
-}
+};
 
 exports.default = buildTs;
