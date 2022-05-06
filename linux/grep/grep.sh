9e27332cba5b043b583e815349f510a5cb53cf65
k22o <kana_t3@yahoo.co.jp>
2022-05-06 18:41:16 +0900

    [update] example

--- linux/grep/grep.sh
+++ linux/grep/grep.sh
@@ -8,4 +8,12 @@ echo 123 456 789 | grep -oE '[7-9]+' # o:マッチする部分を改行して表
 grep '[1-10]' -R # ディレクトリ内のファイル全てをを再帰的に走査
 echo abcde | grep -x 'abc' # 行が完全一致した場合のみ表示
 
-# 
+# regex
+grep -oE '[1-3]{2}.{,3}' sample.txt
+grep -E '^[^s]' sample.txt
+grep -E 'u$' sample.txt
+grep -E 'see?t' sample.txt 
+grep -E 's(a|i)t' sample.txt
+grep -P '\d+' sample.txt
+grep -P '12(?=3)' sample.txt # 肯定の先読み
+grep -P '12(?!3)' sample.txt # 否定の先読み
